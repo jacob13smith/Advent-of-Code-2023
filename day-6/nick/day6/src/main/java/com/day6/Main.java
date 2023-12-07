cfa4c79b4fbf6871be4f959e5b359c153343c187
Nick Hughes <nickhughes17@hotmail.com>
2023-12-06 17:48:19 -0800

    Day6Part2 DONE

--- day-6/nick/day6/src/main/java/com/day6/Main.java
+++ day-6/nick/day6/src/main/java/com/day6/Main.java
@@ -4,24 +4,20 @@ import main.java.com.Converter;
 
 public class Main {
      public static void main(String[] args) {
-        int[] times = {48, 87, 69, 81};
-        int[] distances = {255, 1288, 1117, 1623};
-        int result = 1;
-        for(int i = 0; i < times.length; i++){
-            int numWays = 0;
-            int time = times[i];
-            int dist = distances[i];
-            for(int j = 0; j <= time; j++){
-                int timeHeld = j;
-                int timeRemaining = time - j;
-                int distCovered = timeHeld * timeRemaining;
-                if(distCovered >= dist){
-                    numWays += 1;
-                }
+        long time = 48876981;
+        long distance = 255128811171623L;
+        long result = 1;
+        
+        long numWays = 0;
+        for(long j = 0; j <= time; j++){
+            long timeHeld = j;
+            long timeRemaining = time - j;
+            long distCovered = timeHeld * timeRemaining;
+            if(distCovered >= distance){
+                numWays += 1;
             }
-            System.out.println("NumWays for time " + i + ": " + numWays);
-            result *= numWays;
         }
+        result *= numWays;
         System.out.println(result);
     }
 }
