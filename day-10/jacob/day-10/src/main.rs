852c58f551b5451ea7d6784b13593c1d0db78b7a
Jacob Smith <jacob13smith@gmail.com>
2023-12-13 22:53:32 -0800

    day 10 DONE

--- day-10/jacob/day-10/src/main.rs
+++ day-10/jacob/day-10/src/main.rs
@@ -15,6 +15,7 @@ fn main() {
 
     if let Ok(lines) = read_lines("./input.txt") {
         let mut pipe_map: Vec<Vec<char>> = vec![];
+        let mut visited: HashMap<(i32, i32), bool> = HashMap::new();
         let mut start: (i32, i32) = (0, 0);
 
         let mut line_pos: i32 = 0;
@@ -30,9 +31,8 @@ fn main() {
             line_pos += 1;
         }
 
-        println!("Start: ({}, {})", start.0, start.1);
+        visited.insert((start.0, start.1), true);
 
-        // ((x, y), distance, (entry_x, entry_y)
         let mut probe_0 = (start, 0, (0, 0));
         let mut probe_1 = (start, 0, (0, 0));
 
@@ -43,25 +43,25 @@ fn main() {
             ((-1, 0), "-LF")];
 
         for direction in directions {
+            if start.1 as i32 + direction.0.1 < 0 || start.0 as i32 + direction.0.0 < 0 {
+                continue;
+            }
+
             if direction.1.contains(pipe_map[(start.1 as i32 + direction.0.1) as usize][(start.0 as i32 + direction.0.0) as usize]){
                 if probe_0.1 == 0 {
                     probe_0 = (((start.0 as i32 + direction.0.0), (start.1 as i32 + direction.0.1)), 1, (-direction.0.0, -direction.0.1));
+                    visited.insert((probe_0.0.0, probe_0.0.1), true);
                 } else {
                     probe_1 = (((start.0 as i32 + direction.0.0), (start.1 as i32 + direction.0.1)), 1, (-direction.0.0, -direction.0.1));
+                    visited.insert((probe_1.0.0, probe_1.0.1), true);
                 }
             }
         }
 
-        let mut i = 0;
         while probe_0.0 != probe_1.0 {
-            println!("Probe_0: ({}, {}), Probe_1: ({}, {})", probe_0.0.0, probe_0.0.1, probe_1.0.0, probe_1.0.1);
-            i += 1;
-            // Next probe position = current probe position + directional movement
-            // directional movement = the non-entry direction of the current pipe + 1
-            // non-entry direction = current char direction options - entry direction
+            
             for probe in [&mut probe_0, &mut probe_1].iter_mut() {
                 let pipe = pipe_map[probe.0.1 as usize][probe.0.0 as usize];
-                //println!("Checking: {}", pipe);
                 let entries = *directon_map.get(&pipe).unwrap();
                 let entry_direction = (
                     if probe.2.1 == -1 {1} else {0},
@@ -75,17 +75,37 @@ fn main() {
                     entries.2 - entry_direction.2,
                     entries.3 - entry_direction.3,
                 );
-                //println!("Next direction: ({}, {}, {}, {})", next_direction.0,next_direction.1,next_direction.2,next_direction.3);
         
                 probe.0.0 = probe.0.0 + if next_direction.1 == 1 {1} else if next_direction.3 == 1 {-1} else {0};
                 probe.0.1 = probe.0.1 + if next_direction.0 == 1 {-1} else if next_direction.2 == 1 {1} else {0};
 
                 probe.1 += 1;
                 probe.2 = (if next_direction.1 == 1 {-1} else if next_direction.3 == 1 {1} else {0}, if next_direction.0 == 1 {1} else if next_direction.2 == 1 {-1} else {0});
+                visited.insert((probe.0.0, probe.0.1), true);
             }
         
         }
         println!("Steps: {}", probe_0.1);
+
+
+        let mut tiles = 0;
+        for (y, line) in pipe_map.iter().enumerate() {
+            for x in 0..line.len() {
+                if !visited.contains_key(&(x as i32, y as i32)){
+                    let mut count = 0;
+                    for k in 0..x {
+                        if visited.contains_key(&(k as i32, y as i32)) && "JL|S".contains(line[k]) {
+                            count += 1;
+                        }
+                    }
+                    if count % 2 == 1 {
+                        tiles += 1;
+                    }
+                }
+            }
+        }
+
+        println!("Tiles: {}", tiles);
     }
 }
 
